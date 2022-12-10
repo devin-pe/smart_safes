ae23acddea52cda5fe66e4225af52dcb11656e98
Devin <devpereira1@gmail.com>
2022-12-09 23:30:56 -0500

    Removed unnecessary comments

--- results.py
+++ results.py
@@ -7,17 +7,17 @@ def record_success(pt_path, iters):
         # Decrypt and actaully assign to variables to mimic regular control flow
         flag, pt, obj = decrypt_load('pass') 
     avg_duration = (time.time()-start_time)/iters
-    # short: 0.054322242736816406  medium: 0.055107023239135744  long: 0.05461845254898071
     print(f'Average time taken to decrypt {pt_path} successfully: {avg_duration}')
-    # medium2: 0.452160231590271
+
 
 def record(iters=500):
     """Runs and  all trials"""
 
-    # Run the sucessful
+    
     short_path = 'plaintexts/short.txt'
     medium_path = 'plaintexts/medium.txt'
     long_path = 'plaintexts/long.txt'
+
     #record_success(short_path, iters)
     record_success(medium_path, iters)
     #record_success(long_path, iters)
@@ -32,11 +32,9 @@ def record(iters=500):
         if iter_duration > 0.1:
             min_duration = min(min_duration, iter_duration)
     avg_duration = (time.time()-start_time)/iters
+
     print(f'Average time taken to decrypt unsuccessfully: {avg_duration}')
-    # 0.6694933376312255 - 0.46015363311767576
     print(f'Maximum unsuccessful decryption time: {max_duration}')
-    # 1.0979440212249756 
     print(f'Minimum unsuccessful decryption time: {min_duration}')
-    # 0.05288267135620117 - 0.4163169860839844
 
 record()
